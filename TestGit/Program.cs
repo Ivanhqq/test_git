cc8eed27d0ca35e52556b8eb90c4c796a10610ea
Ivan <ivanazanov@gmail.com>
2022-07-12 21:28:37 +0500

    тестим гит

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
